d1254964048b7109d32af361fc45b15d45f10fd7
Alejandro <alejandromg73@educastur.es>
2025-01-27 09:58:26 +0100

    Probando primera pantalla en el navegador

--- src/main/java/com/alejandromg/tarea3dwes24/controllers/PlantasController.java
+++ src/main/java/com/alejandromg/tarea3dwes24/controllers/PlantasController.java
@@ -2,6 +2,8 @@ package com.alejandromg.tarea3dwes24.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.alejandromg.tarea3dwes24.servicios.ServiciosPlanta;
 
@@ -10,5 +12,10 @@ public class PlantasController {
 	
 	@Autowired
 	private ServiciosPlanta servPlanta;
-
+	
+	 @GetMapping("/plantas")
+	    public String listarPlantas(Model model) {
+	        model.addAttribute("listado_plantas", servPlanta.verTodas());
+	        return "listado_plantas";
+	    }
 }
--- src/main/java/com/alejandromg/tarea3dwes24/controllers/ViveroController.java
+++ src/main/java/com/alejandromg/tarea3dwes24/controllers/ViveroController.java
@@ -1,8 +1,12 @@
 package com.alejandromg.tarea3dwes24.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViveroController {
-	
+	 @GetMapping("/")
+	 public String invitado() {
+	    return "invitado";
+	    }
 }
