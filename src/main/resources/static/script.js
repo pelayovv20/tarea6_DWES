16b549a30d7c8c5a58759ef88a076359f3c8282d
pelayovv20 <pelayovv20educastur.es>
2025-02-13 12:52:27 +0100

    FINAL DEL PROYECTO

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-//Este script es para mostrar el mensaje de confirmación o fallo y que se quite desvaneciendose a los 3 segundos de mostrarse
+//Función para mostrar el mensaje de confirmación o fallo y que se quite desvaneciendose a los 3 segundos de mostrarse
 setTimeout(function() {
         let alertMessages = document.querySelectorAll('.alert');
         alertMessages.forEach(function(alert) {
@@ -7,3 +7,13 @@ setTimeout(function() {
             setTimeout(() => alert.remove(), 500);
         });
     }, 3000);
+	
+	
+//Función para ir a la página anterior
+function goBack() {
+	            if (document.referrer && document.referrer !== window.location.href) {
+	                window.history.back();
+	            } else {
+	                window.location.href = '/invitado'; 
+	            }
+	        }	
